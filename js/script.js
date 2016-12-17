ba33bac8a10c6445cb8bfd6bec3594e915446d8a
Rafael de Oliveira Benetti <rafaeldeoliveirabenetti@gmail.com>
2016-12-16 22:26:49 -0200

    refactor(web font): using sessionStorage to load from cache

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
 var Main = (function () {
 
+  var configureServiceWorker = function() {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker.register('../sw.js');
+      });
+    }
+  }
+
+  var loadFontOnCache = function () {
+    if (sessionStorage.fontsLoaded) {
+      var html = document.documentElement;
+      html.classList.add('fonts-loaded');
+    }
+  }
+
+  var initApplication = function () {
+    configureServiceWorker();
+    loadFontOnCache();
+    FontLoader.init();
+  }
+
   var init = function () {
     document.onreadystatechange = function () {
       if (document.readyState === "complete") {
         initApplication();
       }
     }
-  }
-
-  var initApplication = function () {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', function () {
-        navigator.serviceWorker.register('../sw.js');
-      });
-    }
-
-    FontLoader.init();
   }
 
   return {
